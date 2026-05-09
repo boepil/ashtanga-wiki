0a324d4dddbb984da0383f8a10080b75c29eb0b8
Boaz <boaz@example.com>
2026-05-09 14:56:06 +0300

    Quartz sync: May 9, 2026, 2:56 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Georgia",
+        header: "Lora",
         body: "Inter",
         code: "IBM Plex Mono",
       },
